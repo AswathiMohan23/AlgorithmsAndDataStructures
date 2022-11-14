283b27441c5f6e86bbd505ef7c63f005a50cc905
AswathiMohan23 <achu999here@gmail.com>
2022-11-14 11:25:38 +0530

    [ADDED]

--- src/com/java/dequeueAbstract/PalindromeChecker.java
+++ src/com/java/dequeueAbstract/PalindromeChecker.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class PalindromeChecker {
     static Scanner sc =new Scanner(System.in);
     static Queue queue=new Queue();
+    static StringBuilder reverse;
+
 
     public static void main(String[] args){
         System.out.println("Enter the string : ");
@@ -14,18 +16,25 @@ public class PalindromeChecker {
         String[] firstString=new String[size];
         String[] secondString=new String[size];
 
-
+        //====================================== converting string to char =======================================
         for(int i=0;i<size;i++) {
             convertedWord[i] = word.charAt(i);
             firstString=queue.enqueue(String.valueOf(convertedWord[i]));
         }
         queue.display(firstString);
+        StringBuilder stringBuilder=new StringBuilder();
         for(int i=0;i<size;i++) {
-            secondString = queue.dequeue(firstString[i]);
+           secondString = queue.dequeue(firstString[i]);
         }
-        System.out.println("the reverse string is \n");
+        for(int i=0;i<size;i++){
+            if(secondString[i]!=null) {
+                reverse = stringBuilder.append(secondString[i]);
+            }
+        }
+        System.out.println("the reverse string is \n"+reverse);
         queue.display(secondString);
-        if((firstString).equals(secondString)){
+
+        if((word).equals(reverse.toString())){
             System.out.println("its palindrome");}
         else
             System.out.println("not palindrome");
